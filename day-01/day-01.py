f69bc28857b5f412bd0bdf0cc39105bfa5e9c479
Dvid <51921134+daviduster@users.noreply.github.com>
2021-12-01 12:21:01 +0100

    Update day-01.py

--- day-01/day-01.py
+++ day-01/day-01.py
@@ -2,12 +2,12 @@ depths = open("input.txt").read().split("\n")
 depths = [int(a) for a in depths]
 
 def part_one(depths: list) -> int:
-    return num_increasing(depths)
+    return number_depth_increases(depths)
 
 def part_two(depths: list) -> int:
-    return num_increasing([depths[x] + depths[x - 1] + depths[x - 2] for x in range(2, len(depths))])
+    return number_depth_increases([depths[x] + depths[x - 1] + depths[x - 2] for x in range(2, len(depths))])
 
-def num_increasing(depths: list) -> int:
+def number_depth_increases(depths: list) -> int:
     return sum(depths[x] > depths[x - 1] for x in range(1, len(depths)))
 
 print(part_one(depths))
